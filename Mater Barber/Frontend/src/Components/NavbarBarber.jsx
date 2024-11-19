22b4c6a23e3b65c52d4f3447410af1f682dfc33c
Sergio <163668311+DavidV4iss@users.noreply.github.com>
2024-11-19 16:19:18 -0500

    cambio
    barber
    
    Co-Authored-By: Fidel-Smoke <189048434+Fidel-Smoke@users.noreply.github.com>

--- Mater Barber/Frontend/src/Components/NavbarBarber.jsx
+++ Mater Barber/Frontend/src/Components/NavbarBarber.jsx
@@ -7,28 +7,54 @@ export default function NavbarBarber() {
     window.location.href = "/";
   }; 
   return (
-    <Navbar expand="lg">
-      <Container fluid>
-        {/* Parte izquierda de la Navbar con fondo azul */}
-        <Navbar.Brand as={Link} to="/" className="bg-primary text-white px-3 py-2 rounded">
-          Barber Dashboard
-        </Navbar.Brand>
-        
-        {/* Columna de navegación */}
-        <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto"> {/* ms-auto para alinear la nav a la derecha */}
-            <Nav.Link as={Link} to="/" className="bg-light text-dark px-3 py-2 rounded mx-1">
-              Inicio
-            </Nav.Link>
-            <Nav.Link as={Link} to="/reservations" className="bg-light text-dark px-3 py-2 rounded mx-1">
-              Reservaciones
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-};
+    <div className="navbar border-bottom bg-dark shadow">
+      <div class="container-fluid">
+        <a class="navbar-brand text-warning anton fs-2">
+          <img
+            src="LOGO.png"
+            alt=""
+            width="40"
+            height="40"
+            class="d-inline-block align-text-top mx-4 mt-1"
+          />
+          Master Barber
+        </a>
+        <div class="d-flex ">
+          <div class="dropdown position-absolute top-0 end-0 pe-2 me-2">
+            <button
+              class="btn dropdown text-white d-none d-sm-block"
+              type="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              <small className="d-none d-sm-block mt-1 mx-3 fw-bold text-white">
+                BIENVENIDO ADMINISTRADOR
+              </small>
+              <i class="bi bi-person-circle fs-3"></i>
+            </button>
+            <ul class="dropdown-menu bg-dark">
+              <li>
+                <a class="dropdown-item bebas text-danger" href="/PerfilAdmin">
+                  Perfil
+                </a>
+              </li>
+              <li>
+                <a
+                  class="dropdown-item bebas text-danger"
+                  href="/ConfiguracionAdmin"
+                >
+                  Configuración
+                </a>
+              </li>
+              <li onClick={cerrarSesion}>
+                <a class="dropdown-item text-warning bebas" href="#">
+                  <i class="bi bi-box-arrow-right mx-1"></i> Cerrar sesión
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
