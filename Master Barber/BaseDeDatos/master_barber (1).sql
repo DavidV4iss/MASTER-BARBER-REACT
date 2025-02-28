86b273e2907ceed747681d60f6d25000e2c7b551
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-02-28 06:38:07 -0500

    SQL

--- Master Barber/BaseDeDatos/master_barber (1).sql
+++ Master Barber/BaseDeDatos/master_barber (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-02-2025 a las 13:54:29
+-- Tiempo de generación: 28-02-2025 a las 12:37:02
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,29 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `addbarberos`
---
-
-CREATE TABLE `addbarberos` (
-  `id_barbero` int(11) NOT NULL,
-  `nombre` varchar(200) NOT NULL,
-  `descripcion` varchar(200) NOT NULL,
-  `Foto` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `addbarberos`
---
-
-INSERT INTO `addbarberos` (`id_barbero`, `nombre`, `descripcion`, `Foto`) VALUES
-(11, 'FIdel', 'Cuchillas', 'barbero_1740062486235-MB7.JPG'),
-(12, 'Deiby', 'Cuchillas', 'barbero_1740062498233-B1.JPG'),
-(13, 'Nixxon', 'Cuchillas', 'barbero_1740062517721-B3.JPG'),
-(14, 'Cristian', 'Cuchillas', 'barbero_1740062529873-MB3.JPG');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `calificaciones`
 --
 
@@ -62,8 +39,16 @@ CREATE TABLE `calificaciones` (
 --
 
 INSERT INTO `calificaciones` (`id`, `usuario_id`, `puntuacion`, `comentario`) VALUES
-(3, 5, 2, 'kkkk'),
-(4, 5, 3, 'ddddd');
+(7, 5, 5, 'Excelente Servicio, Ambiente Comodo Y Agradable. Recomendadisismo'),
+(16, 5, 2, 'a'),
+(17, 5, 3, 'afffffffffffffffffffffffffffffffffff'),
+(18, 5, 5, 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'),
+(19, 5, 5, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'),
+(20, 5, 5, 'e'),
+(21, 5, 5, 'dddd'),
+(22, 5, 5, 'w'),
+(23, 5, 4, ''),
+(24, 5, 5, '');
 
 -- --------------------------------------------------------
 
@@ -88,21 +73,6 @@ INSERT INTO `categoria_producto` (`id_categoria_producto`, `categoria`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `disponibilidad`
---
-
-CREATE TABLE `disponibilidad` (
-  `id` int(11) NOT NULL,
-  `barbero_id` int(11) NOT NULL,
-  `fecha` date NOT NULL,
-  `hora_inicio` time NOT NULL,
-  `hora_fin` time NOT NULL,
-  `estado` enum('disponible','ocupado') DEFAULT 'disponible'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `inventario`
 --
 
@@ -113,6 +83,28 @@ CREATE TABLE `inventario` (
   `cantidad` int(255) NOT NULL,
   `id_categoria_producto` int(255) NOT NULL,
   `PrecioUnitario` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`id_producto`, `nombre`, `descripcion_P`, `cantidad`, `id_categoria_producto`, `PrecioUnitario`) VALUES
+(26, 'Camisas', 'negras,blancas y azules', 30, 1, 500000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario_vendido`
+--
+
+CREATE TABLE `inventario_vendido` (
+  `id_producto_vendido` int(11) NOT NULL,
+  `id_categoria_producto` int(11) NOT NULL,
+  `proveedor` varchar(255) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_venta` date NOT NULL,
+  `total_venta` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -129,13 +121,6 @@ CREATE TABLE `reservas` (
   `tipo_servicio_id` int(11) DEFAULT NULL,
   `estado` enum('Pendiente','Aceptada','Cancelada') NOT NULL DEFAULT 'Pendiente'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `reservas`
---
-
-INSERT INTO `reservas` (`id_reserva`, `fecha`, `usuario_id`, `barbero_id`, `tipo_servicio_id`, `estado`) VALUES
-(1, '2024-11-28 21:38:56', 9, 8, 2, 'Aceptada');
 
 -- --------------------------------------------------------
 
@@ -194,30 +179,25 @@ CREATE TABLE `usuarios` (
   `id_rol` int(255) NOT NULL,
   `user_reset_code` varchar(7) DEFAULT NULL,
   `user_reset_code_expiration` datetime DEFAULT NULL,
-  `Foto` varchar(255) DEFAULT NULL
+  `Foto` varchar(255) DEFAULT NULL,
+  `descripcion` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email`, `nit`, `telefono`, `contrasena`, `id_rol`, `user_reset_code`, `user_reset_code_expiration`, `Foto`) VALUES
-(5, 'Fidel Espitia ', 'fideljoseespi10@gmail.com', 1028662003, '3142758305', '$2a$10$Wu5aatNss9/D/N/DQ5v2uuvP9BhDL09q8/v8XQHUHUvjqCxQ6rCKG', 3, NULL, NULL, '1740138744864-LOGO.png'),
-(6, 'Fidel Jose ', 'Admin@gmail.com', 1028662003, '3142758305', '$2a$10$gKkjGOeNlRvXzyePlVJq1.r/9Y.F6.f.UROSSUNuM7Sjv1xkZyRo.', 1, NULL, NULL, '1732824864824-MB3.JPG'),
-(7, 'Julio Cesar', 'jespitiagalvis@gmail.com', 1028662005, '3196524963', '$2a$10$xQyXwmewFti5MfBoxGR06eYSvgqSkKZ9Uc.At9kZnbxyGpYYnw2lG', 3, NULL, NULL, '1732327755702-B1.JPG'),
-(8, 'David Vaiss', 'Barber@gmail.com', 1014481681, '3107877172', '$2a$10$G/u8lp8/i78hNpTwFbjMse9KyDkgx0xnnCQoFwlmdg4Yj6C5Piy4u', 2, NULL, NULL, '1732827233417-logomastershop.png'),
-(9, 'David Vaiss', 'cristianrueda0313@gmail.com', 1014481682, '3044495505', '$2a$10$xywk1zgIsOcpzHqybgjV8e.4YNuOQg5nhvCRvxD5bwXc42kKpTooe', 3, NULL, NULL, '1732827364642-MB1.JPG'),
-(10, 'David', 'cris031331@gmail.com', 1014481683, '3044495506', '$2a$10$zdq6rw8dxvVK19UeIie37uFB/VPbQnaqCIDbtV3A3o2C622578YPu', 3, NULL, NULL, '1740056554991-MB1.JPG');
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `email`, `nit`, `telefono`, `contrasena`, `id_rol`, `user_reset_code`, `user_reset_code_expiration`, `Foto`, `descripcion`) VALUES
+(5, 'Fidel Espitia ', 'fideljoseespi10@gmail.com', 1028662003, '3142758305', '$2a$10$Wu5aatNss9/D/N/DQ5v2uuvP9BhDL09q8/v8XQHUHUvjqCxQ6rCKG', 3, NULL, NULL, '1740579974533-B2.JPG', ''),
+(6, 'Fidel Jose ', 'Admin@gmail.com', 1028662003, '3142758305', '$2a$10$gKkjGOeNlRvXzyePlVJq1.r/9Y.F6.f.UROSSUNuM7Sjv1xkZyRo.', 1, NULL, NULL, '1732824864824-MB3.JPG', ''),
+(7, 'Julio Cesar', 'jespitiagalvis@gmail.com', 1028662005, '3196524963', '$2a$10$xQyXwmewFti5MfBoxGR06eYSvgqSkKZ9Uc.At9kZnbxyGpYYnw2lG', 3, NULL, NULL, '1732327755702-B1.JPG', ''),
+(9, 'David Vaiss', 'cristianrueda0313@gmail.com', 1014481682, '3044495505', '$2a$10$xywk1zgIsOcpzHqybgjV8e.4YNuOQg5nhvCRvxD5bwXc42kKpTooe', 3, NULL, NULL, '1732827364642-MB1.JPG', ''),
+(10, 'David', 'cris031331@gmail.com', 1014481683, '3044495506', '$2a$10$zdq6rw8dxvVK19UeIie37uFB/VPbQnaqCIDbtV3A3o2C622578YPu', 3, NULL, NULL, '1740056554991-MB1.JPG', ''),
+(15, 'Cristian', '', 0, '', '', 2, NULL, NULL, 'barbero_1740665625034-MB1.JPG', 'Cuchillas');
 
 --
 -- Índices para tablas volcadas
 --
-
---
--- Indices de la tabla `addbarberos`
---
-ALTER TABLE `addbarberos`
-  ADD PRIMARY KEY (`id_barbero`);
 
 --
 -- Indices de la tabla `calificaciones`
@@ -233,17 +213,17 @@ ALTER TABLE `categoria_producto`
   ADD PRIMARY KEY (`id_categoria_producto`);
 
 --
--- Indices de la tabla `disponibilidad`
---
-ALTER TABLE `disponibilidad`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `barbero_id` (`barbero_id`);
-
---
 -- Indices de la tabla `inventario`
 --
 ALTER TABLE `inventario`
   ADD PRIMARY KEY (`id_producto`),
+  ADD KEY `id_categoria_producto` (`id_categoria_producto`);
+
+--
+-- Indices de la tabla `inventario_vendido`
+--
+ALTER TABLE `inventario_vendido`
+  ADD PRIMARY KEY (`id_producto_vendido`),
   ADD KEY `id_categoria_producto` (`id_categoria_producto`);
 
 --
@@ -279,16 +259,10 @@ ALTER TABLE `usuarios`
 --
 
 --
--- AUTO_INCREMENT de la tabla `addbarberos`
---
-ALTER TABLE `addbarberos`
-  MODIFY `id_barbero` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
 -- AUTO_INCREMENT de la tabla `calificaciones`
 --
 ALTER TABLE `calificaciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT de la tabla `categoria_producto`
@@ -297,16 +271,16 @@ ALTER TABLE `categoria_producto`
   MODIFY `id_categoria_producto` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT de la tabla `disponibilidad`
---
-ALTER TABLE `disponibilidad`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `inventario`
 --
 ALTER TABLE `inventario`
-  MODIFY `id_producto` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id_producto` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario_vendido`
+--
+ALTER TABLE `inventario_vendido`
+  MODIFY `id_producto_vendido` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `reservas`
@@ -318,7 +292,7 @@ ALTER TABLE `reservas`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id_usuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_usuario` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Restricciones para tablas volcadas
@@ -331,16 +305,16 @@ ALTER TABLE `calificaciones`
   ADD CONSTRAINT `calificaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE;
 
 --
--- Filtros para la tabla `disponibilidad`
---
-ALTER TABLE `disponibilidad`
-  ADD CONSTRAINT `disponibilidad_ibfk_1` FOREIGN KEY (`barbero_id`) REFERENCES `usuarios` (`id_usuario`);
-
---
 -- Filtros para la tabla `inventario`
 --
 ALTER TABLE `inventario`
   ADD CONSTRAINT `producto categoria producto` FOREIGN KEY (`id_categoria_producto`) REFERENCES `categoria_producto` (`id_categoria_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `inventario_vendido`
+--
+ALTER TABLE `inventario_vendido`
+  ADD CONSTRAINT `inventario_vendido_ibfk_1` FOREIGN KEY (`id_categoria_producto`) REFERENCES `categoria_producto` (`id_categoria_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `reservas`
