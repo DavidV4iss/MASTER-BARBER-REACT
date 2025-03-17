16e6b4aeeb900febcd98269ff6296fc084fa7803
DavidV4iss <163668311+DavidV4iss@users.noreply.github.com>
2025-03-17 10:00:47 -0500

    Reserva FRONT

--- Master Barber/Frontend/src/pages/Usuario/InicioUsuario.jsx
+++ Master Barber/Frontend/src/pages/Usuario/InicioUsuario.jsx
@@ -31,18 +31,6 @@ export default function InicioUsuario() {
     comentario: ""
   });
 
-  // useEffect(() => {
-  //   const fetchCalificaciones = async () => {
-  //     try {
-  //       const res = await axios.get("http://localhost:8081/traerCalificaciones");
-  //       setCalificaciones(res.data);
-  //     } catch (err) {
-  //       console.log("Error al obtener las calificaciones:", err);
-  //     }
-  //   };
-  //   fetchCalificaciones();
-  // }, []);
-
   const handleChange = (e) => {
     setNuevaCalificacion(prev => ({ ...prev, [e.target.name]: e.target.value }));
   }
@@ -90,7 +78,7 @@ export default function InicioUsuario() {
   return (
     <div className='Registro'>
       <NavbarUserIndex />
-     
+
       <div
         className="container-fluid p-5 nab table-responsive col col-sm-12"
         id="homeuser"
@@ -101,7 +89,57 @@ export default function InicioUsuario() {
 
         <ReservaCliente />
 
+        <h1 className='text-white text-center mt-5 pt-5 antonparabackend'>¡ Ten en cuenta nuestros dos servicios de corte para poder hacer tu reserva !</h1>
 
+        <div class="row row-cols-1 row-cols-md-2 g-5 mt-4 justify-content-center ">
+          <div class="col w-25 ">
+            <div class="card h-100 bg-black border border-3 rounded-2">
+              <div class="card-body">
+                <div className="text-center">
+                  <img src="/corteBasico.jpg" class="img-fluid rounded-2 card-img mt-3 w-75" alt="..." />
+                </div>
+                <h5 class="card-title text-danger text-center bebas mt-4">Corte basico </h5>
+                <div className="text-center">
+                  <button type="button" className="btn btn-outline-danger mt-5" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">Ver</button>
+                </div>
+              </div>
+              <div class="offcanvas offcanvas-start bg-dark" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
+                <div class="offcanvas-header">
+                  <h5 class=" mt-5 pt-5 text-warning anton" id="staticBackdropLabel">Servicio 1</h5>
+                  <button type="button" class="btn-close mb-5 bg-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body text-white">
+                  <div>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet ex eos pariatur voluptatibus porro nemo harum ducimus excepturi placeat sed illum, iusto dolore expedita minima, eaque nostrum. Rerum, architecto culpa.
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col w-25 ">
+            <div class="card h-100 bg-black border border-3 rounded-2">
+              <div class="card-body">
+                <div className="text-center">
+                  <img src="/cortePremium.jpg" class="img-fluid rounded-2 card-img mt-3 w-75" alt="..." />
+                </div>
+                <h5 class="card-title text-danger text-center bebas mt-4">Corte Premium </h5>
+                <div className="text-center">
+                  <button type="button" className="btn btn-outline-danger mt-5" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">Ver</button>
+                </div>
+              </div>
+              <div class="offcanvas offcanvas-end bg-dark" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                <div class="offcanvas-header">
+                  <h5 class="offcanvas-title text-warning anton mt-5 pt-5" id="offcanvasRightLabel">Servicio 2</h5>
+                  <button type="button" class="btn-close mb-5 bg-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body text-white">
+                  <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur ratione itaque dicta, amet dolorem, ea repellat aut asperiores accusantium quasi explicabo eum ipsam inventore quia ullam culpa. Ad, ab culpa!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
         <div className="container text-white text-center">
           <div className="container-fluid  p-5 mt-5 ">
@@ -129,12 +167,12 @@ export default function InicioUsuario() {
               </div>
               <label className='mx-3 fw-bold mb-3 fs-3 bebas text-warning'>Comentario</label>
               <div>
-                <textarea type="text" name="comentario" className='form-control bg-dark text-white w-50 contenido5 p-5 border-light border-1 fw-bold text-center' value={nuevaCalificacion.comentario} onChange={handleChange}/>
+                <textarea type="text" name="comentario" className='form-control bg-dark text-white w-50 contenido5 p-5 border-light border-1 fw-bold text-center' value={nuevaCalificacion.comentario} onChange={handleChange} />
               </div>
               <button type="submit" className='btn btn-danger mt-3 '>Enviar Calificación</button>
             </form>
           </div>
-      </div>
+        </div>
       </div>
     </div>
 
--- Master Barber/Frontend/src/pages/Usuario/ReservaCliente.jsx
+++ Master Barber/Frontend/src/pages/Usuario/ReservaCliente.jsx
@@ -1,9 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import styled from 'styled-components';
+
+const StyledDatePicker = styled(DatePicker)`
+  width: 435px;
+  height: 40px;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  &:focus {
+    border-color:rgb(255, 0, 0);
+    box-shadow: 0 0 8px rgba(0, 123, 255, 0.25);
+  }
+`;
 
 export default function Reserva() {
     const [service, setService] = useState('');
-    const [date, setDate] = useState('');
+    const [date, setDate] = useState(new Date());
     const [barberoId, setBarberoId] = useState('');
     const [clienteId, setClienteId] = useState(''); // Asume que tienes el ID del cliente de alguna manera
     const [barberos, setBarberos] = useState([]);
@@ -39,41 +56,42 @@ export default function Reserva() {
             barbero_id: barberoId,
             fecha: date
         })
-        .then(response => {
-            if (response.data.disponible) {
-                // Crear la reserva si está disponible
-                const reserva = {
-                    cliente_id: id,
-                    barbero_id: barberoId,
-                    servicio: service,
-                    fecha: date,
-                    estado: 'pendiente'
-                };
+            .then(response => {
+                if (response.data.disponible) {
+                    // Crear la reserva si está disponible
+                    const reserva = {
+                        cliente_id: id,
+                        barbero_id: barberoId,
+                        servicio: service,
+                        fecha: date,
+                        estado: 'pendiente'
+                    };
 
-                axios.post('http://localhost:8081/CrearReservas', reserva)
-                    .then(response => {
-                        console.log(response.data);
-                        alert('Reserva creada exitosamente');
-                    })
-                    .catch(error => {
-                        console.error('Hubo un error al crear la reserva:', error);
-                    });
-            } else {
-                alert('La hora seleccionada ya está ocupada. Por favor, elige otra hora.');
-            }
-        })
-        .catch(error => {
-            console.error('Hubo un error al verificar la disponibilidad:', error);
-        });
+                    axios.post('http://localhost:8081/CrearReservas', reserva)
+                        .then(response => {
+                            console.log(response.data);
+                            alert('Reserva creada exitosamente');
+                        })
+                        .catch(error => {
+                            console.error('Hubo un error al crear la reserva:', error);
+                        });
+                } else {
+                    alert('La hora seleccionada ya está ocupada. Por favor, elige otra hora.');
+                }
+            })
+            .catch(error => {
+                console.error('Hubo un error al verificar la disponibilidad:', error);
+            });
     };
 
     return (
-        <div className='text-white'>
-            <h2>Hacer una Reserva</h2>
+        <div className='text-white text-center mt-5 border border-4 rounded-4 w-50 container '>
+            <h2 className='text-white text-center mt-5 antonparabackend'>¡ Reserva Aqui !</h2>
+            
             <form onSubmit={handleSubmit}>
-                <label>
-                    Tipo de Servicio:
-                    <select value={service} onChange={(e) => setService(e.target.value)}>
+                <label className='text-white mt-4 mb-4 w-50'>
+                    <label className='antonparabackend' htmlFor="">Tipo de Servicio:</label> 
+                    <select className='form-select' value={service} onChange={(e) => setService(e.target.value)}>
                         <option value=''>Seleccione un servicio</option>
                         {servicios.map(servicio => (
                             <option key={servicio.id_tipo_servicio} value={servicio.id_tipo_servicio}>
@@ -83,25 +101,41 @@ export default function Reserva() {
                     </select>
                 </label>
                 <br />
-                <label>
-                    Fecha y Hora:
-                    <input type='datetime-local' value={date} onChange={(e) => setDate(e.target.value)} />
+
+
+                <label className="text-white  ">
+                    <label className='antonparabackend' htmlFor="">Fecha y hora: </label>
+                    <div>
+                    <StyledDatePicker
+                        selected={date}
+                        onChange={(date) => setDate(date)}
+                        showTimeSelect
+                        dateFormat="Pp"
+                        placeholderText="Selecciona una fecha y hora"
+                        />
+                    </div>
                 </label>
                 <br />
-                <label>
-                    Barbero:
-                    <select value={barberoId} onChange={(e) => setBarberoId(e.target.value)}>
-                        <option value=''>Seleccione un barbero</option>
-                        {barberos.map(barbero => (
-                            <option key={barbero.id_usuario} value={barbero.id_usuario}>
-                                {barbero.nombre_usuario}
-                            </option>
-                        ))}
-                    </select>
-                </label>
+
+                <label className='antonparabackend mt-4' htmlFor="">Barbero: </label>
+                <select className="form-select w-50 container " aria-label="Default select example" value={barberoId} onChange={(e) => setBarberoId(e.target.value)}>            
+                    <option value=''>Seleccione su barbero</option>
+                    {barberos.map(barbero => (
+                        <option key={barbero.id_usuario} value={barbero.id_usuario}>
+                            {barbero.nombre_usuario}
+                        </option>
+                    ))}
+                </select>
+
                 <br />
-                <button type='submit'>Reservar</button>
+                <div className='container mb-5 mt-3'>
+                    <button type='submit'>Reservar</button>
+                </div>
             </form>
+
+            
         </div>
+
+        
     );
 }
