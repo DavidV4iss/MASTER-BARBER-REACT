ddee293a4fc1d0f3d208c9fdcd0a738391d33e04
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-04-09 09:48:11 -0500

    Creacion De La Carpeta Master Barber Movil

---
+++ Master Barber/Master Barber - Movil/MasterBarberMovil/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Quuibo Pues Gran Htps Van A Conocer Lo Que Es Programar Grandisimos Hijos De Puta</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
