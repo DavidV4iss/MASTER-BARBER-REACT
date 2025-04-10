888d3836664edb7e4e05c7a5c3c530ac5d0cf27d
Fidel-Smoke <fideljoseespi10@gmail.com>
2025-04-10 07:44:09 -0500

    Footer movil

--- Master Barber/MasterBarberMovil/Master_Barber/App.js
+++ Master Barber/MasterBarberMovil/Master_Barber/App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome'; // Importa la biblioteca de íconos
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: '#ffffff' }}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: '#ffffff' }}>Master Barber</Text>
+      <View style={styles.footer}>
+        <Icon name="home" size={30} color="#ffffff" />
+      </View>
     </View>
   );
 }
@@ -16,5 +19,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#333333',
+    width: '100%',
+    padding: 10,
+    position: 'absolute',
+    bottom: 0,
   },
 });
